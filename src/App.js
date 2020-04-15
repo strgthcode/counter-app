da1ad3fe92990311a8de23d05e6aed9f3b79e127
strgthcode <62224013+strgthcode@users.noreply.github.com>
2020-04-15 08:24:40 -0500

    Basic understanding of useState, add increase,decease,reset buttons and counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Row, Col,  } from 'antd';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function increase(){
+    setCount(count + 1);
+  }
+
+  function decrease(){
+    setCount(count - 1);
+  }
+  
+  function reset(){
+    setCount(0);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={"center"} align={"middle"}>
+        <Col className="gutter-row" span={24} justify={"center"} align={"middle"}>
+          <h1 className="heading">Counter App</h1>
+        </Col>
+      </Row>
+    
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={"center"} align={"middle"}>
+        <Col className="gutter-row" span={24} justify={"center"} align={"middle"}>
+          <h1 className="count">{count}</h1>
+        </Col>
+      </Row>
+
+      
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={"center"} align={"middle"}>
+         <Col className="gutter-row" span={4}>
+          <Button className="button" onClick={decrease}>-</Button>
+        </Col>
+        <Col className="gutter-row" span={4}>
+          <Button className="button" onClick={increase}>+</Button>
+        </Col>
+        <Col className="gutter-row" span={4}>
+            <Button className="button" onClick={reset}>Reset</Button>
+
+        </Col>
+      </Row> 
+
+
+  
+    </>
   );
 }
 
